61b69a66c5c45a8323d7cd5ad0764a75207e9f1a
jonathanli15 <40543169+jonathanli15@users.noreply.github.com>
2022-11-23 20:18:27 -0500

    Update test.py

--- Code/Backend/test.py
+++ Code/Backend/test.py
@@ -4,10 +4,12 @@ import os
 from collections import OrderedDict #allowed to import this?
 import sys
 
+
 os.chdir('../../')
 sourcePath = os.getcwd() + sys.argv[1]
 targetPath = os.getcwd()
 targetList = open(os.getcwd() + sys.argv[2], 'r', encoding='latin1').read().splitlines()
+outputThreshold = float(sys.argv[4]) 
 
 for targetFilename in targetList:
     with open(os.path.join(targetPath, targetFilename), 'r', encoding='latin1') as f: # open in readonly mode
@@ -18,15 +20,15 @@ for targetFilename in targetList:
         with open(os.path.join(sourcePath,sourceFilename), 'r', encoding='latin1') as f:
             sourceData = f.read()
             f.close()
-        result = facade.TraceLinkValue(sourceData,targetData,"word2vec",)
-        # print("Source File: ",sourceFilename, "Target File: ", targetFilename, "Traceability: ",result)
+        result = facade.TraceLinkValue(sourceData,targetData,"word2vec")
 
     # new code added to keep track of/print just the four links with the highest traceability values
         values[result[1]] = sourceFilename
-    final_four = sorted(values)
-    final_four = final_four[len(final_four)-4:] 
-
+    final_out = sorted(values)
+    key=0
     with open(os.getcwd() + sys.argv[3], 'w', encoding='latin1') as writeFile:
-        for key in final_four:
-            print("Source File: " + values[key] + ", Target File: " + targetFilename + ", Traceability: " + str(key) + '\n')
-            writeFile.write("Source File: " + values[key] + ", Target File: " + targetFilename + ", Traceability: " + str(key) + '\n')
+      while float(final_out[key]) >= outputThreshold and key < len(final_out):
+          print("Source File: ",values[final_out[key]], "Target File: ", targetFilename, "Traceability: ",final_out[key])
+          writeFile.write("Source File: " + values[final_out[key]] + ", Target File: " + targetFilename + ", Traceability: " + final_out[key] + '\n')
+          key+=1
+      writeFile.close()
