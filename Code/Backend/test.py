07c1a0b56f3063efc6902315779b100b4e03a8c2
Jonathan <40543169+jonathanli15@users.noreply.github.com>
2022-11-25 14:28:15 -0500

    Update test.py

--- Code/Backend/test.py
+++ Code/Backend/test.py
@@ -1,15 +1,19 @@
 import ds4se.facade as facade
-import pandas as pd
 import os
-from collections import OrderedDict #allowed to import this?
 import sys
 
+
+def new_probability(num1, num2):   #written as a function to be more easily updated to a different algorithm later
+    return (num1+num2)/2
 
 os.chdir('../../')
 sourcePath = os.getcwd() + sys.argv[1]
 targetPath = os.getcwd()
 targetList = open(os.getcwd() + sys.argv[2], 'r', encoding='latin1').read().splitlines()
 outputThreshold = float(sys.argv[4]) 
+
+input = {} #sys.argv[5], the dictionary with (targetFile, sourceFile) -- a tuple -- as the key and the probability as the value
+#dictionary to be filled later
 
 for targetFilename in targetList:
     with open(os.path.join(targetPath, targetFilename), 'r', encoding='latin1') as f: # open in readonly mode
@@ -20,7 +24,12 @@ for targetFilename in targetList:
         with open(os.path.join(sourcePath,sourceFilename), 'r', encoding='latin1') as f:
             sourceData = f.read()
             f.close()
+
         result = facade.TraceLinkValue(sourceData,targetData,"word2vec")
+        tmpStr = targetFilename+", "+sourceFilename
+        if tmpStr in input:  #check if the file pair is in the input from the user
+            result = new_probability(result, input[tmpStr])  #recalculating the probability
+        # print("Source File: ",sourceFilename, "Target File: ", targetFilename, "Traceability: ",result)
 
     # new code added to keep track of/print just the four links with the highest traceability values
         values[sourceFilename] = result[1]
